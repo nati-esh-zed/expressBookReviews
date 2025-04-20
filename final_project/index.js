2501e309485a76fe90d93e5e3ff27679443fcb7c
nati-esh-zed <the.defalt8@gmail.com>
2025-04-20 17:30:31 +0300

    use dotenv + update dependencies ~ update .gitignore to include .env

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,26 @@
+require("dotenv").config({ path: "config/.env" });
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+const PORT = Number.parseInt(process.env.PORT || "5000");
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET;
+
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: SESSION_SECRET, resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  //Write the authenication mechanism here
 });
- 
-const PORT =5000;
+
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
